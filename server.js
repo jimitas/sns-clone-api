88e97adb57adf5cdc4dc9551330e8564d02bfbf0
jimitas <nonajun805@gmail.com>
2023-07-17 16:46:31 +0900

    直接許可

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const app = express();
 const authRoute = require("./routers/auth");
 const postsRoute = require("./routers/posts");
@@ -10,7 +10,14 @@ require("dotenv").config();
 const PORT = process.env.PORT || 10000;
 // const PORT = 5000;
 
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://sns-clone-client.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
